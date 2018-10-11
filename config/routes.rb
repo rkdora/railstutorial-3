0da2c3b17756929870db3900648b4fd028499c58
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-10-11 19:44:52 +0900

    Add activate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
@@ -10,5 +8,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get 'sessions/new'
   resources :users
+  resources :account_activations, only: [:edit]
 end
